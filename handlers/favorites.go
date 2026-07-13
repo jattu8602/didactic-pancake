4c932107ffa53a9bc7c264906f680af58c8b3c2e
jattu8602 <chaurasiyajatin68@gmail.com>
2026-07-13 14:39:38 +0530

    feat: global college marks - show tick if ANY user marked, with who-marked popup

--- handlers/favorites.go
+++ handlers/favorites.go
@@ -8,7 +8,18 @@ import (
 	"github.com/PriyanKishoreMS/colleges-list-api/entities"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type MarkedByUser struct {
+	Name  string `json:"name" bson:"name"`
+	Email string `json:"email" bson:"email"`
+}
+
+type CollegeMark struct {
+	CollegeID string         `json:"collegeId" bson:"collegeId"`
+	MarkedBy  []MarkedByUser `json:"markedBy" bson:"markedBy"`
+}
 
 func (h *APIhandler) ToggleFavorite(c *fiber.Ctx) error {
 	token := extractToken(c)
@@ -30,6 +41,7 @@ func (h *APIhandler) ToggleFavorite(c *fiber.Ctx) error {
 		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token"})
 	}
 
+	// Toggle personal favorite
 	filter := bson.M{"email": user.Email}
 	found := false
 	for _, fid := range user.Favorites {
@@ -44,6 +56,24 @@ func (h *APIhandler) ToggleFavorite(c *fiber.Ctx) error {
 	} else {
 		config.UserCollection.UpdateOne(ctx, filter, bson.M{"$push": bson.M{"favorites": body.CollegeID}})
 	}
+
+	// Toggle global college mark
+	markFilter := bson.M{"collegeId": body.CollegeID}
+	userEntry := MarkedByUser{Name: user.Name, Email: user.Email}
+
+	if found {
+		config.CollegeMarksCollection.UpdateOne(ctx, markFilter, bson.M{"$pull": bson.M{"markedBy": userEntry}})
+	} else {
+		update := bson.M{
+			"$push": bson.M{"markedBy": userEntry},
+			"$setOnInsert": bson.M{"collegeId": body.CollegeID},
+		}
+		opts := options.Update().SetUpsert(true)
+		config.CollegeMarksCollection.UpdateOne(ctx, markFilter, update, opts)
+	}
+
+	// Clean up empty marks
+	config.CollegeMarksCollection.DeleteOne(ctx, bson.M{"collegeId": body.CollegeID, "markedBy": bson.M{"$size": 0}})
 
 	return c.Status(http.StatusOK).JSON(fiber.Map{"favorited": !found})
 }
@@ -61,4 +91,21 @@ func (h *APIhandler) GetFavorites(c *fiber.Ctx) error {
 	}
 
 	return c.Status(http.StatusOK).JSON(fiber.Map{"favorites": user.Favorites})
+}
+
+func (h *APIhandler) GetAllCollegeMarks(c *fiber.Ctx) error {
+	ctx := context.Background()
+
+	cursor, err := config.CollegeMarksCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to fetch marks"})
+	}
+	defer cursor.Close(ctx)
+
+	var marks []CollegeMark
+	if err := cursor.All(ctx, &marks); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to decode marks"})
+	}
+
+	return c.Status(http.StatusOK).JSON(fiber.Map{"marks": marks})
 }
